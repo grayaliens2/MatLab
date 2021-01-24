87812d7ea6569de2e1f8e5f4dca860e20b2f3419
grayaliens2 <77516571+grayaliens2@users.noreply.github.com>
2021-01-23 22:35:03 -0700

    Applies Discrete Sine Transformation to an Image

--- S.m
+++ S.m
@@ -3,6 +3,7 @@ m = zeros(n)
 y = n
 for i = 1 : n%loop goes from 1 to n
     j = 1 : n%unlike the instructions we are going to save alot of time by using 
+    % list comrehension
     %instead of nested loops 
     m(i, j) = sqrt(2/n)*sin((pi*(i-1/2)*(j-1/2))/n) 
 end
